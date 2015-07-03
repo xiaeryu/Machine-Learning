863bd1418d49fc1482e3ddd72c4bfae88c060248
xiaeryu <xiaeryu@nus.edu.sg>
2015-07-03 08:38:29 +0800

    Update kNN.r

--- kNN.r
+++ kNN.r
@@ -1,6 +1,3 @@
-group <- matrix(c(1,1.1,1,1,0,0,0,0.1),ncol=2)
-labels <- c('A','A','B','B')
-
 kNN <- function(inVec,trainSet,trainLabel,k){
         pair_dist <- apply(trainSet,1,function(x) sqrt(sum((inVec - x) ^ 2)))
         bind_dist <- cbind(pair_dist,trainLabel)
