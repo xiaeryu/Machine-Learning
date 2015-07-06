cb0a33ce92b42a85a7cf9ef31263682e976f9936
xiaeryu <xiaeryu@nus.edu.sg>
2015-07-06 20:44:54 +0800

    Create linearRegression.py

---
+++ linearRegression.py
@@ -0,0 +1,88 @@
+from numpy import *
+
+def loadDataSet(fileName):
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr =[]
+        curLine = line.strip().split('\t')
+        labelMat.append(curLine.pop())
+        dataMat.append(curLine)
+    return dataMat,labelMat
+
+def standRegres(xArr,yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    xTx = xMat.T*xMat
+    if linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    return xTx.I * (xMat.T*yMat)
+    
+def lwlr(testPoint,xArr,yArr,k=1.0):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye((m)))
+    for j in range(m):
+        diffMat = testPoint - xMat[j,:]
+        weights[j,j] = exp(diffMat*diffMat.T/(-2.0*k**2))
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    return testPoint * (xTx.I * (xMat.T * (weights * yMat)))
+
+def lwlrTest(testArr,xArr,yArr,k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i],xArr,yArr,k)
+    return yHat
+
+def ridgeRegres(xMat,yMat,lam=0.2):
+    denom = xMat.T*xMat + eye(shape(xMat)[1])*lam
+    if linalg.det(denom) == 0.0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    return denom.I * (xMat.T*yMat)
+
+def ridgeTest(xArr,yArr):
+    xMat = mat(xArr)
+    yMat=mat(yArr).T
+    yMean = mean(yMat,0)
+    yMat = yMat - yMean
+    xMeans = mean(xMat,0)
+    xVar = var(xMat,0)
+    xMat = (xMat - xMeans)/xVar
+    numTestPts = 30
+    wMat = zeros((numTestPts,shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws = ridgeRegres(xMat,yMat,exp(i-10))
+        wMat[i,:]=ws.T
+    return wMat
+
+def stageWise(xArr,yArr,eps=0.01,numIt=100):
+    xMat = mat(xArr)
+    yMat=mat(yArr).T
+    yMean = mean(yMat,0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m,n=shape(xMat)
+    ws = zeros((n,1)); wsTest = ws.copy(); wsMax = ws.copy()
+    for i in range(numIt):
+        print ws.T
+        lowestError = inf;
+        for j in range(n):
+            for sign in [-1,1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps*sign
+                yTest = xMat*wsTest
+                rssE = rssError(yMat.A,yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i,:]=ws.T
+    return returnMat
